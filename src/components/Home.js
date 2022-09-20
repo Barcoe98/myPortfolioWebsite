fbcbacd7eaeae4b84bfb32f83db2fcc4c82fbe3f
MichaelBarcoe <michael_barcoe@fundrecs.com>
2022-09-20 17:09:43 -0400

    initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div className='home'>
+            <div className='content'>
+                <p>Michael Barcoe</p>
+                <p>My Portfolio</p>
+            </div>
+        </div>
+    )
+}
+
+export default Home
