c41b17daecc24275b5bae38cebe95d781b544cbd
MichaelBarcoe <michael_barcoe@fundrecs.com>
2022-09-20 17:48:47 -0400

    added home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import profilePicture from "../images/profilePicture.jpg";
 import './Home.css'
 
 const Home = () => {
     return (
-        <div className='home'>
-            <div className='content'>
-                <p>Michael Barcoe</p>
-                <p>My Portfolio</p>
+        <div className='home' id='home'>
+            <div className='container'>
+                <div className='col-1'>
+                    <img src={profilePicture} alt="Michael Barcoe" />
+                </div>
+                <div className="col-2">
+                    <p>Hi. I'm Michael</p>
+                    <p>A Software Engineer</p>
+                </div>
             </div>
         </div>
     )
